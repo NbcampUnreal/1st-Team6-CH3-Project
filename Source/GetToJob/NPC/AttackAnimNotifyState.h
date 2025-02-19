c2d979352dea6c2b20e74ef1e5fda1dd8c82d632
nonecalculate <escortbayonet@gmail.com>
2025-02-19 19:39:30 +0900

    C++ Codes, BT Blueprints
    BT 에 넣어줄 테스크 C++, 애니메이션 관련 Cpp 과 BT 블루프린트들을 추가했습니다.

---
+++ Source/GetToJob/NPC/AttackAnimNotifyState.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "AttackAnimNotifyState.generated.h"
+
+UCLASS()
+class GETTOJOB_API UAttackAnimNotifyState : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
