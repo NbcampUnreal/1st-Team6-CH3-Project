c81a31fcf199ec218963b0a3a015f8e32b571fba
건강 보조 식품 양배추 <gpwjd7332@naver.com>
2025-02-20 16:55:16 +0900

    인스턴스 코드 추가
    - 생성자에 처음 값 초기화
    - 점수 추가 함수
    - 몬스터 킬 카운트 함수
    - Get으로 변수 가져오는 함수

--- Source/GetToJob/GameManager/GJGameInstance.h
+++ Source/GetToJob/GameManager/GJGameInstance.h
@@ -25,5 +25,42 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "GameData")
 	int32 BossWaveScore; //보스 웨이브 점수
 
+	//점수 추가 함수
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	void AddToScore(int amount);
 
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	void SaveOneWaveScore(int amount);
+
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	void SaveTwoWaveScore(int amount);
+
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	void SaveThreeWaveScore(int amount);
+
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	void BossScore(int amount);
+
+	//전체 적 킬 수
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	void AddToEnemyKill(int amount);
+
+	//점수와 적킬 수 가져오는 함수
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	int32 GetTotalScore();
+
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	int32 GetOneWaveScore();
+
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	int32 GetTwoWaveScore();
+
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	int32 GetThreeWaveScore();
+
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	int32 GetBossScore();
+
+	UFUNCTION(BlueprintCallable, Category = "GameData")
+	int32 GetEnemyTotalKillCount();
 };
