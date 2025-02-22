a407d361b8fb222f1be3a655b73510eb8b1ca795
stone1397 <157870045+stone1397@users.noreply.github.com>
2025-02-22 14:12:57 +0900

    edit Rocket
    - 로켓 데미지 안 들어가는 버그 수정
    (넉백 확인을 위해 BP_GJRocketProjectile의 DamageAmount를 1로 수정했습니다. 데미지를 올리려면 여기를 수정해주세요.)

--- Source/GetToJob/Weapon/GJRocketProjectile.cpp
+++ Source/GetToJob/Weapon/GJRocketProjectile.cpp
@@ -127,7 +127,7 @@ void AGJRocketProjectile::AutoExplode()
 		GetActorLocation(),
 		DamageRadius,
 		nullptr,
-		OverlappedActors,
+		TArray<AActor*>(),
 		this,
 		GetInstigatorController(),
 		true
