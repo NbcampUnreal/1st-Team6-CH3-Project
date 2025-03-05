a520463129359bc8ec869e7dd43513b391a45b94
stone1397 <157870045+stone1397@users.noreply.github.com>
2025-03-05 10:16:38 +0900

    edit AIController, gun
    게이지 증가량 증가
    AI인지 거리 증가

--- Source/GetToJob/Weapon/GJRevolver.cpp
+++ Source/GetToJob/Weapon/GJRevolver.cpp
@@ -131,7 +131,7 @@ void AGJRevolver::Fire()
 						UE_LOG(LogTemp, Warning, TEXT("NPC is Dead! Increasing MiniGun Gauge!"));
 						if (GJCharacter && GJCharacter->MiniGun)
 						{
-							GJCharacter->MiniGun->IncreaseGauge(20.0f);
+							GJCharacter->MiniGun->IncreaseGauge(7.0f);
 						}
 					}
 				}
