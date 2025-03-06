016e64757c4903703e5c1df57882ca3e26a7ece2
stone1397 <157870045+stone1397@users.noreply.github.com>
2025-03-06 20:12:51 +0900

    add shotgun class
    shotgun 클래스를 추가했습니다.
    - GJShotGun은 적에게 쐈을 때 적을 넉백
    - 한 점으로 나가는 것이 아니라 원뿔형으로 근거리에 방사
    - shotgun asset과 sound를 추가했습니다.

---
+++ Source/GetToJob/Weapon/GJShotGun.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/GJBaseGun.h"
+#include "GJShotGun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GETTOJOB_API AGJShotGun : public AGJBaseGun
+{
+	GENERATED_BODY()
+	
+public:
+    AGJShotGun();
+
+protected:
+    virtual void Fire() override;
+
+    // 넉백 힘 (조절 가능)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Shotgun")
+    float KnockbackForce;
+
+    // 샷건 탄환 개수
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Shotgun")
+    int32 PelletCount;
+
+    // 원뿔형 발사 각도
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Shotgun")
+    float SpreadAngle;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Own Weapon")
+    bool bPickShotGun;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+    float TraceRange;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Stats")
+    float CoolDownDelay;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Sound")
+    USoundBase* FireSound;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Particle")
+    UParticleSystem* HitEffect;
+
+    virtual void Pickup(ACharacter* PlayerCharacter) override;
+
+private:
+    bool bCanFire;
+    FTimerHandle FireCooldownTimer;
+
+
+    void EnableFire();
+    
+};
